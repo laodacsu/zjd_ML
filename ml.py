95f20837425301d8efb92d460f5e87659d52f821
laodacsu <591021927@qq.com>
2020-12-23 21:53:21 +0800

    add

--- ml.py
+++ ml.py
@@ -1,3 +1,12 @@
+'''
+Author: zjd
+Date: 2020-11-21 18:47:09
+LastEditTime: 2020-11-21 20:11:47
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \zjd_ML\ml.py
+'''
+
 print(2)
 print(1)
 print(3)
